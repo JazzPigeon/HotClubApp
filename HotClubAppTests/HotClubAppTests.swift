b0313c99a9e8ed603143a1f57cb419fd4fe5929c
Cindy Michalowski <cindymichalowski.tech@gmail.com>
2025-10-13 14:37:23 -0400

    Initial commit

---
+++ HotClubAppTests/HotClubAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotClubAppTests.swift
+//  HotClubAppTests
+//
+//  Created by Cindy Michalowski on 10/13/25.
+//
+
+import Testing
+@testable import HotClubApp
+
+struct HotClubAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
